e2e360a3891e8cf69b4283639fc4ce40cddf60ee
Menushka Weeratunga <tmweeratunga@gmail.com>
2023-06-19 19:35:35 -0400

    Fix for other function not getting passed

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
 import Cookies from 'js-cookie'
 
-const get = (key: string) => {
+const originalGet = Cookies.get
+const originalSet = Cookies.set
+
+const get = (key?: string) => {
   if (typeof document === 'undefined') {
-    return require('next/headers').cookies().get(key).value
+    if (key) {
+      return require('next/headers').cookies().get(key).value
+    } else {
+      return Object.fromEntries(require('next/headers').cookies().getAll().map((cookie: { name: string, value: string }) => [cookie.name, cookie.value]))
+    }
   } else {
-    return Cookies.get(key)
+    if (key) {
+      return originalGet(key)
+    } else {
+      return originalGet()
+    }
   }
 }
 
-export default {
-  ...Cookies,
-  get
+const set = (key: string, value: string) => {
+  if (typeof document === 'undefined') {
+    return require('next/headers').cookies().set(key, value)
+  } else {
+    return originalSet(key, value)
+  }
 }
+
+Cookies.get = get
+Cookies.set = set
+
+export default Cookies
