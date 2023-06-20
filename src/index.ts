21e6edb17ab71b43c0df3e8eca7b5cb96ffaf289
Menushka Weeratunga <tmweeratunga@gmail.com>
2023-06-20 02:04:05 -0400

    Fix undefined error

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const originalSet = Cookies.set
 const get = (key?: string) => {
   if (typeof document === 'undefined') {
     if (key) {
-      return require('next/headers').cookies().get(key).value
+      return require('next/headers').cookies().get(key)?.value
     } else {
       return Object.fromEntries(require('next/headers').cookies().getAll().map((cookie: { name: string, value: string }) => [cookie.name, cookie.value]))
     }
